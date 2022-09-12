dda8a880d22a72a02243247618ca4300b1ff3090
Nico_Fernando <nico.fernando113@gmail.com>
2022-09-12 09:59:50 +0700

    add new entity Employee

---
+++ src/main/java/com/indocyber/gitApp/entity/Employee.java
@@ -0,0 +1,69 @@
+package com.indocyber.gitApp.entity;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String id;
+
+    private String name;
+
+    private String gender;
+
+    private LocalDate birthDate;
+
+    private LocalDate registerDate;
+
+    private String address;
+
+    public Employee() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(LocalDate registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}

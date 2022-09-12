bd55dfbd3232227b9ade2d5b95ad9aa3108ddb1a
Nico_Fernando <nico.fernando113@gmail.com>
2022-09-12 09:47:55 +0700

    Initial commit

---
+++ src/main/java/com/indocyber/gitApp/GitAppApplication.java
@@ -0,0 +1,13 @@
+package com.indocyber.gitApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitAppApplication.class, args);
+	}
+
+}
